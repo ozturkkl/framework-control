0ea94552c4ad7369c3d5e2b01560f1a394620087
kemal <kemal@descript.com>
2026-03-14 04:33:16 -0500

    some other improvements

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { self, nixpkgs }:
+    { nixpkgs, ... }:
     {
       packages.x86_64-linux.default =
         nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/package.nix
