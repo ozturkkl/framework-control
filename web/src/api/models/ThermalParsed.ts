4373474383a82c0355f4d66bbc735dc96a6b4f5b
kemal <kemal@descript.com>
2026-03-14 19:29:38 -0500

    remove the generated code again, don't like to track it in git

--- web/src/api/models/ThermalParsed.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type ThermalParsed = {
-    temps: Record<string, number>;
-    rpms: Array<number>;
-};
-
