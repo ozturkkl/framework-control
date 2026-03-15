061f9065663fea588a0c8f6e329705ccf152119a
kemal <kemal@descript.com>
2026-03-15 02:07:30 -0500

    Simplify the nix setup since now we have a nixpkgs fork

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
   description = "Framework Control - lightweight control surface for Framework devices";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:ozturkkl/nixpkgs/framework-control";
 
   outputs =
     { nixpkgs, ... }:
     {
-      packages.x86_64-linux.default =
-        nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/package.nix
-          { };
-
-      nixosModules.default = import ./nix/module.nix;
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.framework-control;
+      nixosModules.default = import "${nixpkgs}/nixos/modules/services/hardware/framework-control.nix";
     };
 }
